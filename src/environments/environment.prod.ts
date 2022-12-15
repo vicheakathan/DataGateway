ff283f46fa9016c86b338b3962210d43113dc5e0
than vicheaka <112739297+vicheakathan@users.noreply.github.com>
2022-12-15 13:46:12 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   api_url: "https://dinexcloudgateway.azurewebsites.net/",
-  baseUrl: "https://dinexdatagateway.azurewebsites.net/"
+  baseUrl: "https://dinexdatagateway.azurewebsites.net/",
+  SERVER_API_URL: "",
+  DEVELOPER_OPTIONS: true,
+  ENABLE_PROD_MODE: true,
   // api_url: 'https://localhost:7012/'
 };
